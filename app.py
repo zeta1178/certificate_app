94381ef6612ce2a0e43cef507e93072f4ebb18e2
Michael Cruz <michaelcruz@aim.com>
2022-11-07 19:51:48 -0500

    update with pdf generator

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import pdfkit
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+from datetime import date
 import os
 import json
 from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+from streamlit.components.v1 import iframe
 
 load_dotenv()
 
@@ -47,10 +51,59 @@ contract = load_contract()
 
 accounts = w3.eth.accounts
 account = accounts[0]
-student_account = st.selectbox("Select Account", options=accounts)
-certificate_details = st.text_input("Certificate Details", value="FinTech Certificate of Completion")
-if st.button("Award Certificate"):
+
+# new code start here
+# st.set_page_config(layout="centered", page_icon="🎓", page_title="Diploma Generator")
+st.title("🎓 Diploma PDF Generator")
+
+left, right = st.columns(2)
+
+right.write("Here's the template we'll be using:")
+
+right.image("template.png", width=300)
+
+env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
+template = env.get_template("template.html")
+
+left.write("Fill in the data:")
+form = left.form("template_form")
+student = form.text_input("Student name")
+course = form.selectbox(
+    "Choose course",
+    ["Report Generation in Streamlit", "Advanced Cryptography"],
+    index=0,
+)
+grade = form.slider("Grade", 1, 100, 60)
+student_account = form.selectbox("Select Account", options=accounts)
+certificate_details = form.text_input("Certificate Details", value="FinTech Certificate of Completion")
+submit = form.form_submit_button("Generate PDF")
+
+if submit:
     contract.functions.awardCertificate(student_account, certificate_details).transact({'from': account, 'gas': 1000000})
+    html = template.render(
+        student=student,
+        course=course,
+        grade=f"{grade}/100",
+        date=date.today().strftime("%B %d, %Y"),
+    )
+
+    pdf = pdfkit.from_string(html, False)
+    st.balloons()
+
+    right.success("🎉 Your diploma was generated!")
+    # st.write(html, unsafe_allow_html=True)
+    # st.write("")
+    right.download_button(
+        "⬇️ Download PDF",
+        data=pdf,
+        file_name="diploma.pdf",
+        mime="application/octet-stream",
+    )
+
+# student_account = st.selectbox("Select Account", options=accounts)
+# certificate_details = st.text_input("Certificate Details", value="FinTech Certificate of Completion")
+# if st.button("Award Certificate"):
+#     contract.functions.awardCertificate(student_account, certificate_details).transact({'from': account, 'gas': 1000000})
 
 ################################################################################
 # Display Certificate
